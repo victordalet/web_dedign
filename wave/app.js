4c68a4384a2f6577b23d3fef5d95ef983113057d
victordalet <victordalet@protonmail.com>
2021-12-22 13:35:41 +0100

    effet google font

--- wave/app.js
+++ wave/app.js
@@ -2,7 +2,6 @@ const titre = document.querySelector("h1");
 const txt = document.querySelector("p");
 const btn = document.querySelector("button");
 const imgLaptop = document.querySelector(".laptop");
-const allItems = document.querySelector("li");
 
 const TL1 = new TimelineMax({pause : true});
 
@@ -11,7 +10,5 @@ TL1
 .from(txt,1, {opacity : 0}, '-=0.4' )
 .from(btn,1, {opacity : 0}, '-=0.5' )
 .from(imgLaptop,1, {x: 100, opacity : 0}, '-=0.5' )
-.staggerFrom(allItems,1,{y: -50,opacity:0},0.2, '-=1')
-
 
 TL1.play()
